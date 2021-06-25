49790110766ad427dbbc51c2212879b3f40de1fc
vmoth <57469004+vmoth@users.noreply.github.com>
2021-06-25 13:35:31 +0800

    Update config.el

--- config.el
+++ config.el
@@ -91,3 +91,12 @@ https://github.com/hlissner/doom-emacs/issues/2689
 (after! lsp-mode
   (set-lsp-priority! 'clangd 1))  ; ccls has priority 0
 
+(defun set-exec-path-from-shell-PATH ()
+  "Set up Emacs' `exec-path' and PATH environment variable to match that used by the user's shell.
+
+This is particularly useful under Mac OSX, where GUI apps are not started from a shell."
+  (interactive)
+  (let ((path-from-shell (replace-regexp-in-string "[ \t\n]*$" "" (shell-command-to-string "$SHELL --login -i -c 'echo $PATH'"))))
+    (setenv "PATH" path-from-shell)
+    (setq exec-path (split-string path-from-shell path-separator))))
+
